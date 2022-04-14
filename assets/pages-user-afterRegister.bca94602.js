259b505c008435bf4b0e10d3ae8b8b477b0f8a92
PentaTea <2490445193@qq.com>
2022-04-14 20:13:23 +0000

    Deploying to gh-pages from @ 4chao/app@f09a3684d2cb565a44ad9ce363c555181c9b9079 🚀

---
+++ assets/pages-user-afterRegister.bca94602.js
@@ -0,0 +1,1 @@
+import{d as s,H as a,J as t,o,c as d,w as l,l as r}from"./index.08c3bb8b.js";import{a as e}from"./sys.dd02a16c.js";const n=s({setup:s=>(s,n)=>{const u=a(t("sys"),e);return o(),d(u,null,{default:l((()=>[r("div",null,"恭喜你,注册成功,请修改个人信息")])),_:1})}});export{n as default};
---
+++ assets/pages-user-setting.82002397.js
@@ -0,0 +1,1 @@
+import{d as s,H as a,J as o,o as t,c as r}from"./index.08c3bb8b.js";import{a as c}from"./sys.dd02a16c.js";const d=s({setup:s=>(s,d)=>{const e=a(o("sys"),c);return t(),r(e)}});export{d as default};
