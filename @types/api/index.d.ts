9263b2d818e5ef1c16b14f8f57dd1f2233a66958
wofbi <wofbi1@outlook.com>
2022-11-22 00:30:28 +0800

    ✨ 功能(layout): 增加主页卡片数据展示

--- @types/api/index.d.ts
+++ @types/api/index.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.36.1070 on 2022-05-04 22:47:24.
+// Generated using typescript-generator version 2.36.1070 on 2022-11-19 15:15:40.
 
 interface CreateReportDto {
   reason: string
@@ -15,7 +15,7 @@ interface EchoDto {
 }
 
 interface Email {
-  to: string
+  to: string[]
   subject: string
   content: string
 }
@@ -23,6 +23,29 @@ interface Email {
 interface EnumDataDto {
   description: string
   value: string
+}
+
+interface FileDto {
+  id: number
+  uuid: string
+  filename: string
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface ImageDto extends ComponentDto {
+  id: number
+  uuid: string
+  label: string
+  image_file: FileDto
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface ImagesDto extends ComponentBo {
+  images: ImageDto[]
 }
 
 interface InitDto {}
@@ -37,13 +60,8 @@ interface PageDto<M> {
 
 interface Converter<M, N> {}
 
-interface ResponseDto<T> {
-  code: string
-  message: string
-  data: T
-  is_failure: boolean
-  attachments: string[]
-  _failure: boolean
+interface ResultDto<T> {
+  result: T
 }
 
 interface UpvoteDto {
@@ -53,10 +71,15 @@ interface UpvoteDto {
   upvote_num: number
 }
 
-interface CommentDto {
+interface VerificationCodeDto {
+  code: number
+  account: string
+}
+
+interface CommentDto extends RecComponentDto {
   id: number
   uuid: string
-  user: UserBriefInfoDto
+  author: UserBriefInfoDto
   project_id: number
   paragraph: ParagraphDto
   content: string
@@ -71,14 +94,15 @@ interface CommentDto {
   updated_time: Date
 }
 
-interface CommentTopicDto {
+interface CommentTopicDto extends RecComponentDto {
   id: number
   uuid: string
   user_id: number
   project_id: number
   top_id: number
   content: string
-  status: Status
+  businessStatus: BusinessStatus
+  dataStatus: Status
   created_time: Date
   updated_time: Date
 }
@@ -94,11 +118,11 @@ interface CreateSubCommentDto {
   content: string
 }
 
-interface SubCommentDto {
+interface SubCommentDto extends RecComponentDto {
   id: number
   uuid: string
-  user: UserBriefInfoDto
-  replyUser: UserBriefInfoDto
+  author: UserBriefInfoDto
+  target_sub_comment: TargetSubCommentDto
   project_id: number
   paragraph: ParagraphDto
   root_comment_id: number
@@ -111,12 +135,59 @@ interface SubCommentDto {
   updated_time: Date
 }
 
+interface TargetSubCommentDto {}
+
+interface CreateMdhParagraphComponentDto {
+  name: string
+  display_name: string
+  data_schema: any
+  config_schema: any
+}
+
+interface CreateMdhParagraphDto {
+  text_content: string
+  image_ids: number[]
+  paragraph_component_data: { [index: string]: any }
+  template_id: number
+}
+
+interface CreateStarDto {
+  star: number
+}
+
+interface MdhParagraphComponentsDto {
+  mdhParagraphComponents: MdhParagraphComponentDto[]
+}
+
+interface MdhParagraphDto extends ComponentDto, RecComponentDto {
+  id: number
+  uuid: string
+  text_content: string
+  images: ImageDto[]
+  paragraph_data: { [index: string]: ObjectNode }
+  template_id: number
+  user: UserBriefInfoDto
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface UpdateMdhParagraphTemplateDto {
+  content_tags: string[]
+  component_ids: number[]
+  paragraph_config: { [index: string]: any }
+}
+
+interface UpdateParagraphGroupDto {
+  title: string
+}
+
 interface ComponentDto {}
 
 interface CreateParagraphDto {
-  position: number
-  content: string
+  content: { [index: string]: any }
   type: ComponentType
+  pre_position: number
 }
 
 interface CreateReferenceDto {
@@ -134,18 +205,71 @@ interface CreateTextModificationDto {
   content: string
 }
 
-interface ParagraphDto {
+interface MdhParagraphComponentBriefDto extends Serializable {
+  id: number
+  uuid: string
+  name: string
+  version: number
+  status: Status
+  createdTime: Date
+  updatedTime: Date
+}
+
+interface MdhParagraphComponentDto {
+  id: number
+  uuid: string
+  name: string
+  display_name: string
+  data_schema: any
+  config_schema: any
+  version: number
+  is_creative_component: boolean
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface MdhParagraphTemplateDto {
+  id: number
+  uuid: string
+  user_id: number
+  components: MdhParagraphComponentBriefDto[]
+  componentMap: { [index: string]: MdhParagraphComponentDto }
+  config: { [index: string]: any }
+  parent_template_id: number
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface ParagraphDto extends RecComponentDto {
   id: number
   uuid: string
   user_id: number
   project_id: number
-  position: number
+  paragraph_group_id: number
   component: ComponentDto
   component_type: ComponentType
   component_uuid: string
-  status: ParagraphStatus
+  businessStatus: ParagraphStatus
+  dataStatus: Status
   created_time: Date
   updated_time: Date
+}
+
+interface ParagraphGroupDto extends ComponentDto, RecComponentDto {
+  id: number
+  uuid: string
+  user_id: number
+  title: string
+  mdh_paragraph_template: MdhParagraphTemplateDto
+  status: Status
+  created_time: Date
+  updated_time: Date
+}
+
+interface ParagraphsDto {
+  paragraphs: ParagraphDto[]
 }
 
 interface TextDto extends ComponentDto {
@@ -164,44 +288,93 @@ interface TextModificationDto {
   user_id: number
   text_id: number
   content: string
-  status: ModificationStatus
+  dataStatus: Status
+  businessStatus: ModificationStatus
   created_time: Date
   updated_time: Date
+}
+
+interface UpdateParagraphStatusDto {
+  status: ParagraphStatus
 }
 
 interface CreateProjectDto {
   title: string
   description: string
-  tagIds: number[]
-  visibility: Visibility
+  cover_id: number
+  tag_ids: number[]
+  status: ProjectStatus
 }
 
 interface CreateProjectTagDto {
-  name: string
+  id: number
 }
 
-interface ProjectDto {
+interface CreateTagDto {
+  name: string
+  description: string
+}
+
+interface ProjectDto extends RecComponentDto {
   id: number
   title: string
   description: string
-  cover: string;
+  cover: ImageDto
   user: UserBriefInfoDto
   contributors: UserBriefInfoDto[]
-  tags: ProjectTagDto[]
+  paragraph_groups: ParagraphGroupDto[]
+  tags: TagDto[]
+  paragraph_ids: number[]
   coin_num: number
   participant_num: number
   upvote_num: number
-  visibility: Visibility
-  status: Status
-  createdTime: Date
-  updatedTime: Date
+  favorite_num: number
+  paragraph_num: number
+  pend_review_paragraph_num: number
+  review_failed_paragraph_num: number
+  normal_paragraph_num: number
+  hidden_paragraph_num: number
+  deleted_paragraph_num: number
+  banned_paragraph_num: number
+  dataStatus: Status
+  businessStatus: ProjectStatus
+  created_time: Date
+  updated_time: Date
 }
 
-interface ProjectTagDto {
+interface TagDto {
   id: number
   description: string
   uuid: string
   name: string
+}
+
+interface UpdateProjectDto {
+  title: string
+  description: string
+  cover_id: number
+  tag_ids: number[]
+}
+
+interface RecComponentDto extends Serializable {}
+
+interface RecommendDto {
+  recommendItems: RecommendItemDto[]
+}
+
+interface RecommendItemDto extends Serializable {
+  id: number
+  uuid: string
+  userId: number
+  projectId: number
+  paragraphGroupId: number
+  component: RecComponentDto
+  componentType: RecommendType
+  componentUuid: string
+  businessStatus: ParagraphStatus
+  dataStatus: Status
+  createdTime: Date
+  updatedTime: Date
 }
 
 interface CreateUserDto {}
@@ -210,7 +383,8 @@ interface LoginDto {
   account: string
   password: string
   auth_type: AuthType
-  code: string
+  code: number
+  token: string
 }
 
 interface LoginSystemUserDto {
@@ -231,6 +405,7 @@ interface LoginUserDto {
   avatar: string
   signature: string
   nickname: string
+  is_anonymous: boolean
   project_num: number
   comment_num: number
   coin_num: number
@@ -241,6 +416,8 @@ interface LoginUserDto {
   roles: Role[]
   status: Status
   created_time: Date
+  updated_time: Date
+  is_exist: boolean
 }
 
 interface RegisterDto {
@@ -306,7 +483,7 @@ interface UserBriefInfoDto {
   experience_point: number
 }
 
-interface UserDto {
+interface UserDto extends RecComponentDto {
   id: number
   username: string
   gender: Gender
@@ -316,53 +493,47 @@ interface UserDto {
   experience_point: number
 }
 
-interface VerificationCodeDto {
-  code: number
-  account: string
-}
+interface ComponentBo extends Serializable {}
 
-interface EnumDataInterface {
-  value: string
-  description: string
-}
+interface ObjectNode extends ContainerNode<ObjectNode>, Serializable {}
 
-type EnumData =
-  | 'AUTHENTICATION_STATUS'
-  | 'MODIFICATION_STATUS'
-  | 'PARAGRAPH_STATUS'
-  | 'STATUS'
-  | 'VISIBILITY'
-  | 'ACCOUNT_TYPE'
-  | 'AUTH_TYPE'
-  | 'COMPONENT_TYPE'
-  | 'CONTACT_TYPE'
-  | 'GENDER'
-  | 'REPORT_TYPE'
-  | 'ROLE'
-  | 'UPVOTE_TYPE'
+interface Serializable {}
 
-type AuthenticationStatus = 'NORMAL' | 'EXPIRED' | 'INVALID' | 'NEED_REFRESH'
+interface ContainerNode<T> extends BaseJsonNode, JsonNodeCreator {}
 
-type ModificationStatus = 'NORMAL' | 'BANNED' | 'DELETED' | 'PEND_REVIEW' | 'REVIEW_FAILED'
+interface BaseJsonNode extends Serializable {}
 
-type ParagraphStatus = 'NORMAL' | 'BANNED' | 'DELETED' | 'PEND_REVIEW' | 'REVIEW_FAILED'
+interface JsonNodeCreator {}
 
-type Status = 'NORMAL' | 'BANNED' | 'DELETED'
+type Status = 'NORMAL' | 'HIDDEN' | 'BANNED' | 'DELETED'
 
-type Visibility = 'PUBLIC' | 'PRIVATE'
+type UpvoteType = 'COMMENT' | 'COMMENT_TOPIC' | 'PROJECT' | 'TAG' | 'USER' | 'TEXT_MODIFICATION' | 'SUB_COMMENT' | 'REFERENCE'
 
-type AccountType = 'EMAIL' | 'PHONE' | 'USERNAME'
+type BusinessStatus = 'NORMAL'
+
+type ComponentType = 'TEXT' | 'IMAGE' | 'PROJECT' | 'PARAGRAPH' | 'USER' | 'COMMENT' | 'MDH_PARAGRAPH' | 'PARAGRAPH_GROUP'
+
+type ParagraphStatus = 'PEND_REVIEW' | 'REVIEW_PASS' | 'REVIEW_FAILED'
+
+type ModificationStatus = 'PEND_REVIEW' | 'REVIEW_PASS' | 'REVIEW_FAILED'
+
+type ProjectStatus = 'UNPUBLISHED' | 'PUBLISHED'
+
+type RecommendType =
+  | 'COMMENT'
+  | 'COMMENT_TOPIC'
+  | 'PROJECT'
+  | 'TAG'
+  | 'USER'
+  | 'TEXT_MODIFICATION'
+  | 'SUB_COMMENT'
+  | 'REFERENCE'
+  | 'MDH_PARAGRAPH'
+  | 'PARAGRAPH_GROUP'
+  | 'UNKNOWN'
 
 type AuthType = 'PASSWORD' | 'TOKEN' | 'CODE' | 'WECHAT' | 'QQ'
 
-type ComponentType = 'TEXT' | 'IMAGE' | 'PROJECT' | 'PARAGRAPH' | 'USER' | 'COMMENT'
-
-type ContactType = 'EMAIL' | 'PHONE' | 'UNKNOWN'
-
 type Gender = 'MALE' | 'FEMALE' | 'UNKNOWN'
 
-type ReportType = 'COMMENT' | 'COMMENT_TOPIC' | 'PROJECT' | 'PROJECT_TAG' | 'USER' | 'TEXT_MODIFICATION' | 'SUB_COMMENT' | 'REFERENCE'
-
-type Role = 'ANONYMOUS' | 'USER' | 'MANAGER' | 'SYSTEM'
-
-type UpvoteType = 'COMMENT' | 'COMMENT_TOPIC' | 'PROJECT' | 'PROJECT_TAG' | 'USER' | 'TEXT_MODIFICATION' | 'SUB_COMMENT' | 'REFERENCE'
+type Role = 'NONE' | 'ANONYMOUS' | 'USER' | 'STAFF' | 'DEVELOPER' | 'MANAGER' | 'SYSTEM'
--- src/api/index.ts
+++ src/api/index.ts
@@ -12,40 +12,87 @@ const put = factory("PUT");
 type Page = { page?: number; size?: number; last_time?: number };
 
 export const map = {
+  hello: get<{}, { content: string }>()("/hello"),
   paramEcho: get<{ content: string }, EchoDto>()("/echo"),
   pathEcho: get<{ content: string }, EchoDto>()("/echo/{content}"),
+  // 上传文件
+  upload: post<{ file: string }, {}>()("/file"),
+  // 点赞
+  upvote: post<{}, UpvoteDto>()("/upvote/{upvoteTypeName}/{upvoteId}"),
+  //  举报
+  report: post<{}, {}>()("/report/{reportTypeName}/{reportId}"),
+  // 获取指定enum的内容
+  getEnum: get<{}, {}>()("/enum/{enumName}"),
+  // 获取enum的列表
+  getEnumList: get<{}, {}>()("/enum_list"),
+  // 匿名账户登录
+  anonymousLogin: post<LoginDto, LoginUserDto>()("/anonymous/login"),
   // 登录
   login: post<LoginDto, LoginUserDto>()("/login"),
   // 注册
   register: post<CreateUserDto, UserDto>()("/register"),
   // 获取验证码
-  getVerificationCode: get<{ account: string }, VerificationCodeDto>()("/verification_code"),
-  // 更新本人的信息
-  updateSelfInfo: put<UpdateUserDto, UserDto>()("/self"),
+  getVerificationCode: get<{ account: string }, {}>()("/verification_code"),
+  // 检验验证码
+  verifyVerificationCode: post<VerificationCodeDto, ResultDto<boolean>>()("/verification_code"),
+  // 检验验证码并根据账户存在与否进行登录注册
+  verifyVerificationCodeLoginRegister: get<VerificationCodeDto, LoginUserDto>()("/verification_code"),
+  // 更新当前登录者的信息
+  updateSelf: put<UpdateUserDto, UserDto>()("/self"),
+  // 获取当前登录者的信息
+  getSelf: get<{}, UserDto>()("/self"),
   // 获取其他人的信息
-  getUserInfo: get<{ id: number }, UserDto>()("/user/{id}"),
+  getUser: get<{}, UserDto>()("/user/{id}"),
+  getUserExist: get<{}, ResultDto<boolean>>()("/user/username/{username}/exist"),
+  // 创建文本段落的修改
+  createParaTextModification: post<CreateTextModificationDto, TextModificationDto>()("/text/{textId}/text_modification"),
+
   // 创建项目的标签
-  createProjectTag: post<CreateProjectTagDto, ProjectTagDto>()("/project_tag"),
+  createTag: post<CreateTagDto, TagDto>()("/tag"),
+  // // 创建项目的标签
+  // createProjectTag: post<CreateProjectTagDto, TagDto>()("/project/{projectId}/tag"),
+  // 获取项目的推荐
+  getProjectRecommend: get<{ size: number; offset: number }, RecommendDto>()("/project"),
   // 创建项目
   createProject: post<CreateProjectDto, ProjectDto>()("/project"),
   // 获取项目详情
   getProject: get<{}, ProjectDto>()("/project/{id}"),
   // 获取项目页
-  getProjectPage: get<Page, PageDto<ProjectDto>>()("/project/page/"),
+  getProjectPage: get<Page, PageDto<ProjectDto>>()("/project/page"),
   // 创建项目段落
   createProjectParagraph: post<CreateParagraphDto, ParagraphDto>()("/project/{project_id}/paragraph"),
   // 获取项目的段落
   getProjectParagraph: get<Page, PageDto<ParagraphDto>>()("/project/{project_id}/paragraph/page"),
+  // 更新段落状态
+  updateParagraphStatus: put<UpdateParagraphStatusDto, ParagraphDto>()("/project/{project_id}/paragraph/{paragraph_id}/status"),
+
   // 创建项目评论
   createProjectComment: post<CreateCommentDto, CommentDto>()("/project/{project_id}/comment"),
   // 创建项目子评论
-  createProjectSubComment: post<CreateSubCommentDto, CommentDto>()("/project/{project_id}/comment/{comment_id}/sub_comment"),
+  createProjectSubComment: post<CreateSubCommentDto, CommentDto>()("/project/{project_id}/comment/{comment_id}/sub_comment/{targetSubCommentId}"),
   // 获取项目评论页
-  getProjectCommentPage: get<Page, PageDto<CommentDto>>()("/project/{project_id}/comment/page/"),
+  getProjectCommentPage: get<Page, PageDto<CommentDto>>()("/project/{project_id}/comment/page"),
+  // 获取段落评论页
+  getParagraphCommentPage: get<Page, PageDto<CommentDto>>()("/project/{project_id}/paragraph/{paragraph_id}/comment/page"),
   // 获取项目子评论页
   getProjectSubCommentPage: get<Page, PageDto<CommentDto>>()("/project/{project_id}/comment/{comment_id}/sub_comment/page/"),
-  // 点赞
-  upvote: post<{ upvote_type: UpvoteType }, UpvoteDto>()("/upvote/{upvote_type}/{upvote_id}"),
-  // 举报
-  report: post<{}, {}>()("/report/{report_type}/{report_id}"),
+  // 获取评论两人讨论
+  getConversationPage: get<Page & { interlocutor_ids: number[] }, PageDto<CommentDto>>()("/project/{project_id}/comment/{root_comment_id}/conversation_page"),
+  // 获取多维段落组件列表
+  getMdhParagraphComponents: get<{}, MdhParagraphComponentsDto[]>()("/mdh_paragraph_component"),
+  // 创建多维段落
+  createMdhParagraph: post<CreateMdhParagraphDto, MdhParagraphDto>()("/project/{project_id}/paragraph_group/{paragraph_group_id}/mdh_paragraph"),
+  // 更新多维段落模板配置
+  updateMdhParagraphTemplate: put<UpdateMdhParagraphTemplateDto, MdhParagraphTemplateDto>()("/mdh_paragraph_template/{mdh_paragraph_template_id}"),
+  // 更新段落组配置
+  updateParagraphGroup: put<UpdateParagraphGroupDto, ParagraphGroupDto>()("/project/{project_id}/paragraph_group/{paragraph_group_id}"),
+};
+
+export const manage_map = {
+  // 登录
+  managerLogin: post<LoginDto, LoginUserDto>()("/manage/login"),
+  // 获取段落页
+  managerGetParagraphPage: post<Page & { type?: ParagraphStatus }, PageDto<ParagraphDto>>()("/manage/paragraph/page"),
+  // 创建多维段落组件
+  createMdhParagraphComponent: post<CreateMdhParagraphComponentDto, MdhParagraphComponentDto>()("/manage/mdh_paragraph_component"),
 };
